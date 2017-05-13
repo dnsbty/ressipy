3052dc4c2383a9ca21e5a1c47af1934633166b1f
Dennis Beatty <dennis@dcbeatty.com>
2017-05-13 00:59:01 -0600

    remove npm scripts from upgrade hook

--- upgrade.exs
+++ upgrade.exs
@@ -1,11 +1,6 @@
 defmodule Ressipy.UpgradeCallbacks do
   import Gatling.Bash
 
-  def before_mix_digest(env) do
-    bash("npm", ~w[install], cd: env.build_dir)
-    bash("npm", ~w[run deploy], cd: env.build_dir)
-  end
-
   def before_upgrade_service(env) do
     bash("mix", ~w[ecto.migrate], cd: env.build_dir)
   end
