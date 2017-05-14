df2097f28cb9bd4ddc7dfce1194b2d86329b376a
Dennis Beatty <dennis@dcbeatty.com>
2017-05-14 02:53:18 -0600

    compile javascript in assets folder

--- upgrade.exs
+++ upgrade.exs
@@ -2,8 +2,9 @@ defmodule Ressipy.UpgradeCallbacks do
   import Gatling.Bash
 
   def before_mix_digest(env) do
-    bash("npm", ~w[install], cd: env.build_dir)
-    bash("npm", ~w[run deploy], cd: env.build_dir)
+    assets_path = Path.join([env.build_dir, "assets"])
+    bash("npm", ~w[install], cd: assets_path)
+    bash("npm", ~w[run deploy], cd: assets_path)
   end
 
   def before_upgrade_service(env) do
