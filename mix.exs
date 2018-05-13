aa02046bf97772fc13cabd2916f0879c02932e94
Dennis Beatty <dennis@dcbeatty.com>
2018-05-13 03:54:34 -0600

    actually get httpoison

--- mix.exs
+++ mix.exs
@@ -28,15 +28,18 @@ defmodule Ressipy.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0-rc"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.2"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:distillery, "~> 1.0"}]
+    [
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:distillery, "~> 1.0"},
+      {:httpoison, "~> 1.1"},
+      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
