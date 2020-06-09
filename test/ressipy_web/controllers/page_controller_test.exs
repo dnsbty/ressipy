94e35dfe07c734ca9a48f761214088d949f2c4da
Dennis Beatty <dennis@dcbeatty.com>
2020-06-09 16:55:59 -0600

    fix tests

--- test/ressipy_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RessipyWeb.PageControllerTest do
-  use RessipyWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
