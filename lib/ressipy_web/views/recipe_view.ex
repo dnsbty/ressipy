126238c6292d032b5193e47405dd8ca83c876057
Dennis Beatty <dennis@dcbeatty.com>
2020-11-03 08:45:20 -0700

    only include ids when present
    Right now we're including an ID field at times when we probably
    shouldn't, and that's causing some issues with saving. This will now
    only include ID fields when an ID is actually present.

--- lib/ressipy_web/views/recipe_view.ex
+++ lib/ressipy_web/views/recipe_view.ex
@@ -1,6 +1,11 @@
 defmodule RessipyWeb.RecipeView do
   use RessipyWeb, :view
 
+  @spec has_id?(Phoenix.HTML.Form.t()) :: boolean()
+  def has_id?(form) do
+    !!form.data.id
+  end
+
   def for_select(categories) do
     Enum.map(categories, &{&1.name, &1.id})
   end
