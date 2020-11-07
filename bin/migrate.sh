1e63bff4744ce55afc7e338729b91bcdbf6dee60
Dennis Beatty <dennis@dcbeatty.com>
2020-11-07 15:23:52 -0700

    run migrations with the latest image
    Previously whenever migrations ran, they would just use the image that
    was already in place. This will pass the image ID to the migration
    script so that it will run the migrations from the latest version of the
    Docker image.

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+sed -i "s/{{IMAGE_TAG}}/$1/g" k8s/migrations.yaml
 kubectl apply -f k8s/migrations.yaml >/dev/null
 kubectl wait --for=condition=complete --timeout=1m jobs/ressipy-migrator >/dev/null
 kubectl logs jobs/ressipy-migrator
